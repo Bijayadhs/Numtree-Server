7b1f3dd482b8f85233da180a46efc8ddb83eb606
Bijayadhs <bijayadhikari107@gmail.com>
2020-10-12 08:58:42 +1100

    controller logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { createOrUpdateUser } = require('../controllers/authController')
 
-router.get('/create-or-update-user', (req, res) => {
-    res.json({
-        data: 'Api running...'
-    })
-})
+router.get('/create-or-update-user',)
 
 module.exports = router;
